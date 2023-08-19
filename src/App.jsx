40bec99c7b837f6f9312f6e5d29a3b997d7c815a
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-19 16:28:14 +0200

    template

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="page">
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className="form">
+          <input
+            type="text"
+            className="input"
+            placeholder="Escribe aquí tu película favorita"
+          />
+          <button type="submit" className="button">
+            Buscar
+          </button>
+        </form>
+
+        <main>Lista de peliculas</main>
+      </header>
+    </div>
+  );
 }
 
 export default App;
